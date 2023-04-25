4e079c58415cac232dddf36d4f809875d11d106c
carmengarcia-19 <camegarcia1994@gmail.com>
2023-04-24 18:52:05 -0600

    first commit

---
+++ view/DetalleTicket/index.php
@@ -0,0 +1,125 @@
+<?php
+	require_once("../config/conexion.php");
+	if(isset($_SESSION["usu_id"])){
+?>
+
+
+<!DOCTYPE html>
+<html>
+<?php require_once("../MainHead/head.php");?>
+    <title>Detalle Ticket</title>
+
+<body class="with-side-menu">
+
+    <?php require_once("../MainHeader/header.php");?>   
+
+	<div class="mobile-menu-left-overlay"></div>
+	
+    <?php require_once("../MainNav/nav.php");?>   
+
+	<!--Contenido-->
+
+	<div class="page-content">
+		<div class="container-fluid">
+		<header class="section-header">
+				<div class="tbl">
+					<div class="tbl-row">
+						<div class="tbl-cell">
+							<h3 id="lblnomidticket"></h3>
+							
+							<div id="lblestado"></div>
+							<span class="label label-pill label-primary" id="lblnomusuario"></span >
+							<span class="label label-pill label-default" id="lblfechcrea"></span>
+
+
+							<ol class="breadcrumb breadcrumb-simple">
+								<li><a href="#">Home</a></li>
+								<li class="active">Detalle Ticket</li>
+							</ol>
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<div class="box-typical box-typical-padding">
+				<div class="row">
+				<div class="col-lg-6">
+					<fieldset class="form-group">
+					<label class="form-label semibold" for="cat_nom">Categoría</label>
+					<input id="cat_nom" name="cat_nom" class="form-control" readonly>
+										
+					</fieldset>
+				</div>
+
+				<div class="col-lg-6">
+								<fieldset class="form-group">
+									<label class="form-label semibold" for="tick_titulo">Título</label>
+									<input type="text" class="form-control" id="tick_titulo" name="tick_titulo" readonly>
+								</fieldset>
+				</div>
+
+				<div class="col-lg-12">
+					<fieldset class="form-group">
+					<label class="form-label semibold" for="tickd_descripusu">Descripción</label>
+					
+					<div class="summernote-theme-1">
+						<textarea id="tickd_descripusu" name="tickd_descripusu" class="summernote" name="name" ></textarea>
+					</div>	
+
+				</div>						
+					</fieldset>
+				</div>
+							
+					</form>
+				</div><!--.row-->
+			</div>
+		
+		<section class="activity-line" id="lbldetalle">
+
+
+		</section>
+
+		<div class="box-typical box-typical-padding" id="pnldetalle">
+				
+					<p>
+						Ingrese su duda o consulta
+					</p>
+				<div class="row">
+						
+						<div class="col-lg-12">
+							<fieldset class="form-group">
+								<label class="form-label semibold" for="tickd_descrip">Descripción</label>
+								<div class="summernote-theme-1">
+									<textarea id="tickd_descrip" name="tickd_descrip" class="summernote" name="name" ></textarea>
+								</div>						
+							</fieldset>
+						</div>
+						<div class="col-lg-12">
+						<button type="button" id="btnenviar" class="btn btn-rounded btn-primary">Enviar</button>
+						<button type="button" id="btncerrarticket" class="btn btn-rounded btn-warning">Cerrar Ticket</button>
+
+					
+					</div>
+				</div><!--.row-->
+
+            </div>
+
+		</div>
+	</div>
+	<!--Contenido-->
+
+
+    <?php require_once("../MainJs/js.php");?>
+	<script type="text/javascript" src="detalleticket.js"></script>
+	
+
+</body>
+</html>
+
+
+<?php
+	} else {
+	header("Location:".Conectar::ruta()."view/index.php");
+	}
+
+?>
